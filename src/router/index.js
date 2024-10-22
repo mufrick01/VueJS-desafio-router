013b4bd5a287571b2525748e55b5112983e89875
mufrick01 <mrmufrick@gmail.com>
2024-10-22 19:29:13 -0300

    transicion entre vistas del router mejorada

--- src/router/index.js
+++ src/router/index.js
@@ -34,7 +34,11 @@ const routes = [
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
-  routes
+  routes,
+  scrollBehavior() {
+    return { top: 0, left: 0 };
+  },
+
 })
 
 export default router
